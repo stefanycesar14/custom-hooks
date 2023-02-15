79641f2ad1b8aad5e73bdbdb5868b05d0dedfa52
Stefany <stefany123cesar@gmail.com>
2023-02-15 12:32:01 +0100

    todoReducer added

---
+++ useTodo/todoReducer.js
@@ -0,0 +1,30 @@
+
+export const todoReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [
+                ...state,
+                action.payload
+                
+
+            ]
+        case 'REMOVE_TODO':
+            return state.filter(todo => todo.id!==action.payload)
+  
+       
+        case 'TOGGLE_TODO':
+
+           return state.map(todo=>{
+                if(todo.id === action.payload){
+                  return{
+                    ...todo,
+                    done:!todo.done
+                  }
+
+                }
+                return todo
+            })
+
+    }
+    return state
+}
